b3e2b140a228182333af4bacfbda23ac5a763d38
Czipperz <czipperz@gmail.com>
2014-12-26 20:08:05 -0800

    Attempted to remake some of the changes I forgot to push.

--- src/com/czipperz/blogspot/scrollingMoba/Player.java
+++ src/com/czipperz/blogspot/scrollingMoba/Player.java
@@ -1,5 +1,6 @@
 package com.czipperz.blogspot.scrollingMoba;
 
+import com.czipperz.cLibrary.game.CGameFrame;
 import com.czipperz.cLibrary.game.CObject;
 import com.czipperz.cLibrary.game.IDrawAble;
 import com.czipperz.cLibrary.location.CRect;
@@ -10,7 +11,8 @@ import java.awt.*;
  * Created by Chris on 12/12/2014.
  */
 public class Player extends CObject {
-    public Player() {
+    public Player(CGameFrame displayOn) {
+        super(displayOn);
         loc = new CRect();
     }
 
--- src/com/czipperz/blogspot/scrollingMoba/Start.java
+++ src/com/czipperz/blogspot/scrollingMoba/Start.java
@@ -1,6 +1,7 @@
 package com.czipperz.blogspot.scrollingMoba;
 
 import com.czipperz.cLibrary.game.CGameFrame;
+import com.czipperz.cLibrary.game.CView;
 
 /**
  * Created by Chris on 12/7/2014.
@@ -9,12 +10,14 @@ public class Start {
     public static void main(String[] args) {
         new Start();
     }
-
     public CGameFrame frame;
     public Player player;
 
     public Start() {
         frame = new CGameFrame();
-
+        frame.setSize(500, 500);
+        CView view = new CView(frame);
+        player = new Player(frame);
+        view.addToDraw(player);
     }
 }
